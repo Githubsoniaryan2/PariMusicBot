d8a2b181c13f2251cb27e7f9765af53e1daf43ee
Animal Janwar <160995009+Githubsoniaryan2@users.noreply.github.com>
2025-10-27 11:58:42 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
-DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
+DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 1000))
 
 # Chat id of a group for logging bot's activities
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", None))
@@ -29,9 +29,9 @@ OWNER_ID = int(getenv("OWNER_ID", None))
 # Fill Queue Limit . Example - 15
 QUEUE_LIMIT = int(getenv("QUEUE_LIMIT", "10"))
 
-API_URL = getenv("API_URL", 'https://pytdbotapi.thequickearn.xyz')
+API_URL = getenv("API_URL", 'https://teaminflex.xyz')
 VIDEO_API_URL = getenv("VIDEO_API_URL", 'https://api.video.thequickearn.xyz')
-API_KEY = getenv("API_KEY", 'NxGBNexGenBots07a627')
+API_KEY = getenv("API_KEY", 'INFLEX93454428D')
 
 
 ## Fill these variables if you're deploying on heroku.
@@ -42,7 +42,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Githubsoniaryan2/PariMusicBot",
+    "https://github.com/ARYANSONIOP/YukkiMusicBot",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
@@ -182,6 +182,14 @@ if SUPPORT_GROUP:
         raise SystemExit(
             "[ERROR] - Your SUPPORT_GROUP url is wrong. Please ensure that it starts with https://"
         )
+
+
+
+
+
+
+
+
 
 
 
